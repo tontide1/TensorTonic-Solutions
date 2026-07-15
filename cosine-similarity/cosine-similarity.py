bae7162f259c289854d473e037a3ddda6e0b4ce2
Phan Tấn Tài <124888314+tontide1@users.noreply.github.com>
2026-07-15 23:59:22 +0700

    Solve cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -6,14 +6,12 @@ def cosine_similarity(a, b):
     Returns: float in [-1, 1]
     """
     # Write code here
-    a = np.array(a)
-    b = np.array(b)
-    
-    tu_so = a@b
-    mau_so = np.linalg.norm(a) * np.linalg.norm(b)
-    res = tu_so / mau_so
-    if mau_so == 0:
+    up = np.dot(a,b)
+    down = np.linalg.norm(a) * np.linalg.norm(b)
+
+    if down == 0:
         return 0
     else:
+        res = up / down
         return res
     pass
